5ade77c0d51946002330337d032b9d3ef695db82
vamst <vamst@users.noreply.github.com>
2017-01-27 18:53:44 +0100

    Update Workflow.py

--- cosmos/models/Workflow.py
+++ cosmos/models/Workflow.py
@@ -615,7 +615,7 @@ def _run_queued_and_ready_tasks(task_queue, workflow):
     # ready_tasks = [task for task, degree in list(task_queue.in_degree().items()) if
     #               degree == 0 and task.status == TaskStatus.no_attempt]
     
-    ready_tasks = [task for task, degree in task_queue.in_degree().items() if
+    ready_tasks = [task for task, degree in list(task_queue.in_degree()) if
                     degree == 0 and task.status == TaskStatus.no_attempt]
 
     if max_cores is None:
