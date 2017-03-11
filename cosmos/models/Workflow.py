13fb6efed085377560e4f8e378125d130f6f3c1c
vamst <vamst@users.noreply.github.com>
2017-03-11 15:18:59 +0100

    Update Workflow.py

--- cosmos/models/Workflow.py
+++ cosmos/models/Workflow.py
@@ -146,7 +146,8 @@ class Workflow(Base):
     max_attempts = Column(Integer, default=1)
     info = Column(MutableDict.as_mutable(JSONEncodedDict))
     # recipe_graph = Column(PickleType)
-    _status = Column(Enum34_ColumnType(WorkflowStatus)(255), default=WorkflowStatus.no_attempt)
+    #_status = Column(Enum34_ColumnType(WorkflowStatus)(255), default=WorkflowStatus.no_attempt)
+    _status = Column(String(255), default=WorkflowStatus.no_attempt)
     stages = relationship("Stage", cascade="all, merge, delete-orphan", order_by="Stage.number", passive_deletes=True,
                           backref='workflow')
 
