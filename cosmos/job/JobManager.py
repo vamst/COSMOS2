b4c18033e645ef2625da2556024292f4d27618ac
amstisla <amstisla@molgen.mpg.de>
2017-04-27 16:11:21 +0200

    ROLLBACK

--- cosmos/job/JobManager.py
+++ cosmos/job/JobManager.py
@@ -108,14 +108,14 @@ class JobManager(object):
 
         drm = 'mxq'
         for task, job_info_dict in self.get_drm('mxq').filter_is_done(self.running_tasks):
-            if job_info_dict['status'] != 'inq':
-                print('[DEBUG]')
-                print(task)
-                print(job_info_dict)
-                self.running_tasks.remove(task)
-                for k, v in list(job_info_dict.items()):
-                    setattr(task, k, v)
-                yield task
+            if job_info_dict['status'] == 'inq': continue
+            print('[DEBUG]')
+            print(task)
+            print(job_info_dict)
+            self.running_tasks.remove(task)
+            for k, v in list(job_info_dict.items()):
+                setattr(task, k, v)
+            yield task
 
     @property
     def poll_interval(self):
