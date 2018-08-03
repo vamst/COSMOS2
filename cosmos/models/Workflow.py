6ecc666215717304efff9056a50ea49b3303ddf8
vamst <vamst@users.noreply.github.com>
2018-08-03 14:59:33 +0200

    always recognize dindel tasks as successful

--- cosmos/models/Workflow.py
+++ cosmos/models/Workflow.py
@@ -598,6 +598,8 @@ def _run(workflow, session, task_queue):
 
         for task in _process_finished_tasks(workflow.jobmanager):
             # print("Current task: {}".format(task))
+            if  'dindel' in task.uid:
+                task.status == TaskStatus.successful
             if task.status == TaskStatus.failed and task.must_succeed:
 
                 if workflow.info['fail_fast']:
@@ -612,7 +614,7 @@ def _run(workflow, session, task_queue):
                 task_queue.remove_nodes_from(remove_nodes)
                 workflow.status = WorkflowStatus.failed_but_running
                 workflow.log.info('%s tasks left in the queue' % len(task_queue))
-            elif 'dindel' in task.uid or task.status == TaskStatus.successful:
+            elif task.status == TaskStatus.successful:
                 # just pop this task
                 task_queue.remove_node(task)
             elif task.status == TaskStatus.no_attempt:
